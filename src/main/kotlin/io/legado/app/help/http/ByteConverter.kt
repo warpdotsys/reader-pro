bda1ead9f9b3e453a133841c8046bbe6c57919f2
Transwarpcom <141041258+Transwarpcom@users.noreply.github.com>
2026-07-17 22:22:42 +0800

    Fix Retrofit converter override

--- src/main/kotlin/io/legado/app/help/http/ByteConverter.kt
+++ src/main/kotlin/io/legado/app/help/http/ByteConverter.kt
@@ -3,13 +3,12 @@ package io.legado.app.help.http
 import okhttp3.ResponseBody
 import retrofit2.Converter
 import retrofit2.Retrofit
-import java.lang.annotation.Annotation
 import java.lang.reflect.Type
 
 class ByteConverter : Converter.Factory() {
     override fun responseBodyConverter(
-        type: Type?,
-        annotations: Array<Annotation>?,
-        retrofit: Retrofit?
-    ): Converter<ResponseBody, ByteArray> = Converter { value -> value.bytes() }
+        type: Type,
+        annotations: Array<Annotation>,
+        retrofit: Retrofit
+    ): Converter<ResponseBody, *> = Converter<ResponseBody, ByteArray> { value -> value.bytes() }
 }
