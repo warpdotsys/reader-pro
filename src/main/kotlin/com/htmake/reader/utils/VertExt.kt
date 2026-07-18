6cd7117365b9db500d6ae4ef4a3ae47ea4f71d50
Transwarpcom <141041258+Transwarpcom@users.noreply.github.com>
2026-07-18 17:02:59 +0800

    Correct VertExt JAR boundary

--- src/main/kotlin/com/htmake/reader/utils/VertExt.kt
+++ src/main/kotlin/com/htmake/reader/utils/VertExt.kt
@@ -1,28 +1,47 @@
 package com.htmake.reader.utils
 
-import java.io.File
-import java.nio.file.Paths
+import com.htmake.reader.entity.BasicError
+import io.legado.app.utils.GSON
+import io.vertx.core.Handler
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.Route
+import io.vertx.ext.web.RoutingContext
+import mu.KotlinLogging
+import org.slf4j.MDC
+import java.net.URLDecoder
+import java.util.UUID
 
-var workDirPath = ""
-var workDirInit = false
+private val logger = KotlinLogging.logger {}
 
-fun getWorkDir(subPath: String = ""): String {
-    if (!workDirInit && workDirPath.isEmpty()) {
-        val osName = System.getProperty("os.name")
-        val currentDir = System.getProperty("user.dir")
-        workDirPath = if (osName.startsWith("Mac OS", true) && !currentDir.startsWith("/Users/")) {
-            Paths.get(System.getProperty("user.home"), ".reader").toString()
-        } else {
-            currentDir
-        }
-        workDirInit = true
-    }
-    return Paths.get(workDirPath, subPath).toString()
+fun RoutingContext.success(any: Any?) {
+    val json = if (any is JsonObject) any.toString() else GSON.toJson(any)
+    response().putHeader("content-type", "application/json; charset=utf-8").end(json)
 }
 
-fun getWorkDir(vararg subDirFiles: String): String = getWorkDir(getRelativePath(*subDirFiles))
+fun RoutingContext.error(throwable: Throwable) {
+    val path = URLDecoder.decode(request().absoluteURI(), "UTF-8")
+    val error = BasicError(
+        "Internal Server Error",
+        throwable.toString(),
+        throwable.message.toString(),
+        path,
+        500,
+        System.currentTimeMillis()
+    )
+    val errorJson = GSON.toJson(error)
+    logger.error("Internal Server Error", throwable)
+    logger.error { errorJson }
+    response().putHeader("content-type", "application/json; charset=utf-8")
+        .setStatusCode(500)
+        .end(errorJson)
+}
 
-fun getRelativePath(vararg subDirFiles: String): String {
-    val path = subDirFiles.filter { it.isNotEmpty() }.joinToString(File.separator, prefix = File.separator)
-    return path.removePrefix(File.separator)
+fun Route.globalHandler(handler: Handler<RoutingContext>) {
+    this.handler { context ->
+        val traceId = context.get<String>("traceId").takeUnless { it.isNullOrEmpty() }
+            ?: UUID.randomUUID().toString().substring(0, 8)
+        MDC.put("traceId", traceId)
+        context.put("traceId", traceId)
+        handler.handle(context)
+    }
 }
