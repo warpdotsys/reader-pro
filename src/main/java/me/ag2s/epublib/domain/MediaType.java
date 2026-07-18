037ac9bad9cea4cadc2bdf97576679db901189db
Transwarpcom <141041258+Transwarpcom@users.noreply.github.com>
2026-07-18 14:27:53 +0800

    Restore MediaType generic signature

--- src/main/java/me/ag2s/epublib/domain/MediaType.java
+++ src/main/java/me/ag2s/epublib/domain/MediaType.java
@@ -8,21 +8,21 @@ public class MediaType implements Serializable {
    private static final long serialVersionUID = -7256091153727506788L;
    private final String name;
    private final String defaultExtension;
-   private final Collection extensions;
+   private final Collection<String> extensions;
 
    public MediaType(String name, String defaultExtension) {
       this(name, defaultExtension, new String[]{defaultExtension});
    }
 
    public MediaType(String name, String defaultExtension, String[] extensions) {
-      this(name, defaultExtension, (Collection)Arrays.asList(extensions));
+      this(name, defaultExtension, Arrays.asList(extensions));
    }
 
    public int hashCode() {
       return this.name == null ? 0 : this.name.hashCode();
    }
 
-   public MediaType(String name, String defaultExtension, Collection mextensions) {
+   public MediaType(String name, String defaultExtension, Collection<String> mextensions) {
       this.name = name;
       this.defaultExtension = defaultExtension;
       this.extensions = mextensions;
@@ -36,7 +36,7 @@ public class MediaType implements Serializable {
       return this.defaultExtension;
    }
 
-   public Collection getExtensions() {
+   public Collection<String> getExtensions() {
       return this.extensions;
    }
 
